5b2b2fbe6fbd22ee8373b922c27a71ca62c65083
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-10 19:06:15 +0000

    Deploy ayarhlaine/hotel_template to github.com/ayarhlaine/hotel_template.git:gh-pages

---
+++ precache-manifest.af1aa14cd1afb7912a58052712eeca08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08ab732be7918f6104da1889013231ac",
+    "url": "/hotel_template/index.html"
+  },
+  {
+    "revision": "324e9b869d6c421fee59",
+    "url": "/hotel_template/static/css/2.6d4ecc00.chunk.css"
+  },
+  {
+    "revision": "de865b3bdf60a4ec1ac9",
+    "url": "/hotel_template/static/css/main.796a31d1.chunk.css"
+  },
+  {
+    "revision": "324e9b869d6c421fee59",
+    "url": "/hotel_template/static/js/2.12457b48.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/hotel_template/static/js/2.12457b48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de865b3bdf60a4ec1ac9",
+    "url": "/hotel_template/static/js/main.11eb29bd.chunk.js"
+  },
+  {
+    "revision": "0192b356eb64ba3cd62a",
+    "url": "/hotel_template/static/js/runtime-main.94e78656.js"
+  }
+]);
